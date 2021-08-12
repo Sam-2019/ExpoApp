9117c589080627b28b056bbcf514fa4a7692c1b8
Sam-2019 <samtmartey@gmail.com>
2021-08-12 22:20:21 +0100

    added Main

---
+++ Main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import Navigation from "./navigation";
+
+export default function Main() {
+
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
+}
