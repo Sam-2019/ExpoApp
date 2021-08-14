97025096de57c65d6dbee94318eba662d8d233eb
Sam-2019 <samtmartey@gmail.com>
2021-08-14 20:49:28 +0100

    added auth funcs

--- actions/actions.js
+++ actions/actions.js
@@ -1,4 +1,5 @@
 import { rootStore } from "../store/root";
+import { login as authLogin, resetPassword } from "./auth";
 
 export const { userStore } = rootStore;
 
@@ -6,15 +7,15 @@ export const increment = () => {
   userStore.add();
 };
 
-export const login = () => {
+export const login = ({ userName, password }) => {
   userStore.login();
+  authLogin({ userName, password });
 };
 
 export const logout = () => {
   userStore.logout();
 };
 
-
 export const forgotPassword = () => {
- 
+  resetPassword({ email });
 };
