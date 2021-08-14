517391ed4f8925f1724654a768a78fad9f054423
Sam-2019 <samtmartey@gmail.com>
2021-08-14 17:03:50 +0100

    added FirstTimePage

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import React from "react";
-import { observer } from "mobx-react-lite";
-import { StoreProvider } from "./store/root";
-import Main from "./Main";
+import React, { useEffect } from "react";
 
-const App = observer(() => {
+import FirstTimePage from "./components/FirstTime";
+import Main from "./Main";
+import { StoreProvider } from "./store/root";
+
+import { useAsyncStorage } from "./hooks/useAsyncStorage";
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useAsyncStorage("registerAppOnDevice");
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        setTimeout(() => {
+          setAppIsReady(true);
+        }, 5000);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (appIsReady === undefined) {
+    return <FirstTimePage />;
+  }
+
   return (
     <StoreProvider>
       <Main />
     </StoreProvider>
   );
-});
-
-export default App;
+}
