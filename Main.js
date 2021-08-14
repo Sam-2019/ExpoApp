e499bb76421fd3b2d94b46a8e22aadcd2c5e8f6c
Sam-2019 <samtmartey@gmail.com>
2021-08-14 17:04:23 +0100

    moved StatusBar up

--- Main.js
+++ Main.js
@@ -5,11 +5,10 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./navigation/navigation";
 
 export default function Main() {
-
   return (
     <SafeAreaProvider>
-      <Navigation />
       <StatusBar />
+      <Navigation />
     </SafeAreaProvider>
   );
 }
