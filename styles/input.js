47f6df065dcb8a38fde44355864feb64db0d3c55
Sam-2019 <samtmartey@gmail.com>
2021-08-16 17:11:48 +0100

    removed marginBottom

--- styles/input.js
+++ styles/input.js
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 export const inputStyle = StyleSheet.create({
   input: {
     height: 40,
-    marginBottom: 10,
     borderWidth: 1,
     padding: 10,
   },
