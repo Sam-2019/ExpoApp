0850c559392218bf842336520ca561e511df40a4
Sam-2019 <samtmartey@gmail.com>
2021-08-16 18:05:51 +0100

    auth update

--- actions/auth.js
+++ actions/auth.js
@@ -1,27 +1,16 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
 import Parse, { ParserInit } from "../core/config/parse";
-import { rootStore } from "../store/root";
-
-export const { userStore } = rootStore;
-
-
+import { userStore } from "../actions/actions";
 
 ParserInit();
 
-export const signup = async ({
-  firstName,
-  lastName,
-  username,
-  email,
-  password,
-}) => {
+export const signup = async (values) => {
   const user = new Parse.User();
-  user.set("firstName", firstName);
-  user.set("lastname", lastName);
-  user.set("username", username);
-  user.set("email", email);
-  user.set("password", password);
+  user.set("firstname", values.firstname);
+  user.set("lastname", values.lastname);
+  user.set("username", values.username);
+  user.set("email", values.email);
+  user.set("password", values.password);
 
   try {
     await user.signUp();
@@ -31,22 +20,24 @@ export const signup = async ({
   }
 };
 
-export const login = async ({ username, password }) => {
+export const login = async (values) => {
+  const loggedIn = true;
+
   try {
-    const user = await Parse.User.logIn({ username, password });
-
-    userStore.userDetails({user, loggedIn = true});
-
-    console.log(user);
+    const { attributes } = await Parse.User.logIn(
+      values.email,
+      values.password
+    );
+    userStore.userDetails(attributes, loggedIn);
   } catch (error) {
     console.log("Error: ", error);
   }
 };
 
-export const resetPassword = async ({ email }) => {
+export const resetPassword = async (values) => {
   try {
-    const user = await Parse.User.requestPasswordReset({ email });
-    console.log(user);
+    const { attributes } = await Parse.User.requestPasswordReset(values.email);
+    console.log(attributes);
   } catch (error) {
     console.log("Error: ", error);
   }
@@ -54,20 +45,26 @@ export const resetPassword = async ({ email }) => {
 
 export const currentUser = async () => {
   try {
-    const user = await Parse.User.currentAsync();
-    userStore.userDetails({user, loggedIn = true});
-    console.log(user);
+    const { attributes } = await Parse.User.currentAsync();
+    userStore.userDetails(attributes);
+    console.log(attributes);
   } catch (error) {
     console.log("Error: ", error);
   }
 };
 
 export const logout = async () => {
-  try {
-    const user = await Parse.User.logOut();
-    userStore.userDetails({user,loggedIn = false});
+  const data = await AsyncStorage.getAllKeys();
+  const keys = [data[1], data[2]];
+  // console.log(keys);
 
-    console.log(user);
+  const loggedIn = false;
+  try {
+    const { attributes } = await Parse.User.logOut();
+    userStore.userDetails(attributes, loggedIn);
+    await AsyncStorage.multiRemove(keys);
+
+    console.log(attributes);
   } catch (error) {
     console.log("Error: ", error);
   }
