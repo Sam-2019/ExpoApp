da468c57d095fcce63f8985a45474d6e3f8aae29
Sam-2019 <samtmartey@gmail.com>
2021-08-16 17:11:30 +0100

    added color & fontSize

--- styles/text.js
+++ styles/text.js
@@ -3,13 +3,17 @@ import { StyleSheet } from "react-native";
 export const textStyle = StyleSheet.create({
   loadingText: {
     fontSize: 30,
-    textAlign: 'center'
+    textAlign: "center",
   },
   header: {
     // textAlign: "center",
     marginBottom: 10,
   },
   forgotPassword: {
-    textAlign: 'right'
-  }
+    textAlign: "right",
+  },
+  errorText: {
+    fontSize: 10,
+    color: "red"
+  },
 });
